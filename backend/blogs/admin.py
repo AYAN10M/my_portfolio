dc00b46f8a84d16e08897f6800129edc9d9781f6
AYAN10M <gopalchandrahaldar1997@gmail.com>
2026-03-27 11:11:06 +0530

    Separating frontend and backend

--- backend/blogs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Blog
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'published', 'date', 'created_at')
-    list_filter = ('published', 'date')
-    search_fields = ('title', 'excerpt')
-    prepopulated_fields = {'slug': ('title',)}
--- backend/projects/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Project
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'featured', 'year', 'created_at')
-    list_filter = ('featured', 'year')
-    search_fields = ('title', 'short_desc')
-    prepopulated_fields = {'slug': ('title',)}
